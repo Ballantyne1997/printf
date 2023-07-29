93f11821e92e1c15fef2c377357f16a7959ed190
Ballantyne1997 <thobaniballantyne@icloud.com>
2023-07-28 19:41:29 -0700

    Printf group task

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@
  * Return: the number of characters printed
  */
 
-int _printf(const char * format, ...)
+int _printf(const char *format, ...)
 {
 	unsigned int i, count = 0;
 
@@ -18,7 +18,7 @@ int _printf(const char * format, ...)
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
-        {
+	{
 		if (format[i] != '%')
 		{
 			teamputchar(format[i]);
@@ -43,8 +43,7 @@ int _printf(const char * format, ...)
 		}
 
 		count += 1;
-        }
-	
+	}
 	va_end(args);
 	return (count);
 }
